d13bb47a16d0a93bb6d40f4a06915e3bb18d5e24
Bangkaew-creator <phd02bangknew@gmail.com>
2026-07-01 22:45:14 +0700

    Update config.js
    เปลี่ยนฐานข้อมูลเป็น firebase

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
 // ไฟล์ตั้งค่าส่วนกลางสำหรับเชื่อมต่อระบบหน้าบ้าน (Frontend)
-const LIFF_ID = "2010500776-6r1shNwL"; 
-const API_URL = "https://script.google.com/macros/s/AKfycbyzPy7sklwURcJiqFDzlK4OEQ8616CJLuNyAjb8euQNzYJLXz_dj4UJM63w8-nI1L3r/exec";
+const LIFF_ID = ""2010500776-6r1shNwL"; 
+// ลิงก์ API เดิม เอาไว้ใช้สำหรับส่งแจ้งเตือน LINE
+const API_URL = "https://script.google.com/macros/s/AKfycbyzPy7sklwURcJiqFDzlK4OEQ8616CJLuNyAjb8euQNzYJLXz_dj4UJM63w8-nI1L3r/exec"; 
+
+// ----------------------------------------------------
+// ตั้งค่ากุญแจเชื่อมต่อ Firebase (ฐานข้อมูลใหม่)
+// ----------------------------------------------------
+const firebaseConfig = {
+  apiKey: "AIzaSyBb9rmXVT9A3TFlNHeUu2pp9o8li2WQdik",
+  authDomain: "orsomo-smart.firebaseapp.com",
+  projectId: "orsomo-smart",
+  storageBucket: "orsomo-smart.firebasestorage.app",
+  messagingSenderId: "1090261739518",
+  appId: "1:1090261739518:web:9e7c9eef5a1ad4f3ad82af"
+};
+
