6966f9eedec5186ebaacb5b316b5474b1e7c0abe
Bangkaew-creator <phd02bangknew@gmail.com>
2026-07-01 22:52:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ไฟล์ตั้งค่าส่วนกลางสำหรับเชื่อมต่อระบบหน้าบ้าน (Frontend)
-const LIFF_ID = ""2010500776-6r1shNwL"; 
+const LIFF_ID = "2010500776-6r1shNwL"; 
 // ลิงก์ API เดิม เอาไว้ใช้สำหรับส่งแจ้งเตือน LINE
 const API_URL = "https://script.google.com/macros/s/AKfycbyzPy7sklwURcJiqFDzlK4OEQ8616CJLuNyAjb8euQNzYJLXz_dj4UJM63w8-nI1L3r/exec"; 
 
